b95b7bc5d2a5c21f80a1213b8660189ba2650841
Bloquet Gabin <gabin.bloquet@decathlon.com>
2022-02-13 16:27:08 +0100

    fix: should typo :pencil:

--- src/test/java/io/github/gabbloquet/tddtraining/RomanNumerals/RomanNumeralsTest.java
+++ src/test/java/io/github/gabbloquet/tddtraining/RomanNumerals/RomanNumeralsTest.java
@@ -9,21 +9,21 @@ public class RomanNumeralsTest {
   RomanNumerals romanNumerals = new RomanNumerals();
 
   @Test
-  void shoud_convert_to_I() {
+  void should_convert_to_I() {
     assertEquals("I", romanNumerals.toRoman(1));
     assertEquals("II", romanNumerals.toRoman(2));
     assertEquals("III", romanNumerals.toRoman(3));
   }
 
   @Test
-  void shoud_convert_to_V() {
+  void should_convert_to_V() {
     assertEquals("IV", romanNumerals.toRoman(4));
     assertEquals("V", romanNumerals.toRoman(5));
     assertEquals("VII", romanNumerals.toRoman(7));
   }
 
   @Test
-  void shoud_convert_to_X() {
+  void should_convert_to_X() {
     assertEquals("IX", romanNumerals.toRoman(9));
     assertEquals("X", romanNumerals.toRoman(10));
     assertEquals("XII", romanNumerals.toRoman(12));
@@ -32,7 +32,7 @@ public class RomanNumeralsTest {
   }
 
   @Test
-  void shoud_convert_to_L() {
+  void should_convert_to_L() {
     assertEquals("XL", romanNumerals.toRoman(40));
     assertEquals("L", romanNumerals.toRoman(50));
     assertEquals("LXI", romanNumerals.toRoman(61));
@@ -40,28 +40,28 @@ public class RomanNumeralsTest {
   }
 
   @Test
-  void shoud_convert_to_C() {
+  void should_convert_to_C() {
     assertEquals("XC", romanNumerals.toRoman(90));
     assertEquals("C", romanNumerals.toRoman(100));
     assertEquals("CC", romanNumerals.toRoman(200));
   }
 
   @Test
-  void shoud_convert_to_D() {
+  void should_convert_to_D() {
     assertEquals("CD", romanNumerals.toRoman(400));
     assertEquals("D", romanNumerals.toRoman(500));
     assertEquals("DCC", romanNumerals.toRoman(700));
   }
 
   @Test
-  void shoud_convert_to_M() {
+  void should_convert_to_M() {
     assertEquals("CM", romanNumerals.toRoman(900));
     assertEquals("M", romanNumerals.toRoman(1000));
     assertEquals("MM", romanNumerals.toRoman(2000));
   }
 
   @Test
-  void shoud_convert_to_complexe_numbers() {
+  void should_convert_to_complexe_numbers() {
     assertEquals("DCCCXLVII", romanNumerals.toRoman(847));
     assertEquals("MLIII", romanNumerals.toRoman(1053));
     assertEquals("MDCCLXXVI", romanNumerals.toRoman(1776));
