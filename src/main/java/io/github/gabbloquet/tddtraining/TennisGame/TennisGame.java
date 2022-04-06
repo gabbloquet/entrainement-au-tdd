3c8b742957e1ae3197100fe9207975404c27a594
Bloquet Gabin <gabin.bloquet@decathlon.com>
2022-04-06 14:59:04 +0200

    chore(tennisScore): carry a running game

--- src/main/java/io/github/gabbloquet/tddtraining/TennisGame/TennisGame.java
+++ src/main/java/io/github/gabbloquet/tddtraining/TennisGame/TennisGame.java
@@ -1,12 +1,19 @@
 package io.github.gabbloquet.tddtraining.TennisGame;
 
+import com.google.common.base.CharMatcher;
+
+import java.util.Map;
+
 public class TennisGame {
 
-  public TennisGame(String match) {
+  private final int playerAPoints;
+  private final Map<Integer, String> pointsCorrespondence = Map.of(0, "0", 1, "15", 2, "30", 3, "40");
 
+  public TennisGame(String match) {
+    this.playerAPoints = CharMatcher.is('A').countIn(match);
   }
 
   public String getScore() {
-    return "A 0 0 0 0 0 0 B 0 0 0 0 0 0";
+    return "A 0 0 0 0 0 " + pointsCorrespondence.get(playerAPoints) + " B 0 0 0 0 0 0";
   }
 }
--- src/test/java/io/github/gabbloquet/tddtraining/TennisGame/TennisGameTest.java
+++ src/test/java/io/github/gabbloquet/tddtraining/TennisGame/TennisGameTest.java
@@ -11,4 +11,16 @@ public class TennisGameTest {
     Assertions.assertEquals("A 0 0 0 0 0 0 B 0 0 0 0 0 0", tennisGame.getScore());
   }
 
+  @Test
+  void should_score_a_running_game() {
+    TennisGame aWonApoint = new TennisGame("A");
+    Assertions.assertEquals("A 0 0 0 0 0 15 B 0 0 0 0 0 0", aWonApoint.getScore());
+
+    TennisGame aWonTwoPoints = new TennisGame("A,A");
+    Assertions.assertEquals("A 0 0 0 0 0 30 B 0 0 0 0 0 0", aWonTwoPoints.getScore());
+
+    TennisGame aWonThreePoints = new TennisGame("A,A,A");
+    Assertions.assertEquals("A 0 0 0 0 0 40 B 0 0 0 0 0 0", aWonThreePoints.getScore());
+  }
+
 }
